22675d96541e2965a6e4710a8867b58b20fdbe1c
Rodrigo <rod.maruya@gmail.com>
2025-02-07 12:22:52 +0900

    adicionando arquivo sitemap.xml

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://curiosidade-sem-limites.vercel.app", // URL do seu site
-  generateRobotsTxt: true, // Gera automaticamente o robots.txt
-  sitemapSize: 5000, // Divide sitemaps se houver mais de 5000 URLs
-  generateIndexSitemap: true, // Garante a criação do sitemapindex.xml
-};
