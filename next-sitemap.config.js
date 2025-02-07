3b9fecd1a0115866495ca476aeb58f955b5360cd
Rodrigo <rod.maruya@gmail.com>
2025-02-07 11:23:26 +0900

    🐛 fix: Bugfix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://curiosidade-sem-limites.vercel.app",
-  generateRobotsTxt: true,
+  siteUrl: "https://curiosidade-sem-limites.vercel.app", // URL do seu site
+  generateRobotsTxt: true, // Gera um arquivo robots.txt
+  sitemapSize: 5000, // Número máximo de URLs por sitemap
+  generateIndexSitemap: true, // Gera um sitemapindex.xml automaticamente
 };
